659cd8bebd8aac2938dba85a7401fc4c35807199
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-08-29 21:30:46 +0200

    Create travellerRoutes.js

---
+++ routes/api/travellerRoutes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+
+const { Location, Traveller, Trips } = require('../../models');
+
+// GET all travellers
+
+router.get('/', async (req, res) => {
+    try {
+      const travellerData = await Traveller.findAll({
+        include: [{ model: Traveller }],
+      });
+      res.status(200).json(travellerData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+// CREATE a new traveller record
+
+router.post('/', async (req, res) => {
+    try {
+      const travellerData = await Traveller.create({
+        reader_id: req.body.reader_id,
+      });
+      res.status(200).json(travellerData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+//   GET the data of a single traveller by ID
+
+router.get('/:id', async (req, res) => {
+    try {
+      const travellerData = await Traveller.findByPk(req.params.id, {
+        include: [{ model: Traveller }],
+      });
+  
+      if (!travellerData) {
+        res.status(404).json({ message: 'No traveller found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(travellerData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // DELETE a traveller
+router.delete('/:id', async (req, res) => {
+    try {
+      const travellerData = await Traveller.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!travellerData) {
+        res.status(404).json({ message: 'No traveller found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(travellerData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
